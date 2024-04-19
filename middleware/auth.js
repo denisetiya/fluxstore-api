dbfdbcb9ad9e43ac28cef08951b649e17c8a9c3f
Deni Setiya <144502025+denisetiya@users.noreply.github.com>
2024-04-19 22:21:03 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ const response = require("../utils/response.js");
 auth.use(cookieParser());
 
 auth.use((req, res, next) => {
-  if (req.path === "/user/login" || req.path === "/auth/google" || req.path === "/api-login" || req.path === "/api-docs" || req.path === "/user/forgetPassword" || req.path === "/auth/google/callback" || req.path === "/user/register") {
+  if (req.path === "/user/login" || req.path === "/auth/google" || req.path === "/api-login" || req.path === "/user/forgetPassword" || req.path === "/auth/google/callback" || req.path === "/user/register") {
     return next();
   }
   const token = req.cookies ? req.cookies.token : null;
